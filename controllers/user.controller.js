572576d1e544562697bc4460c56dfc8f3dcec0d3
avinashankur <avinashhankur@gmail.com>
2024-12-12 21:15:42 +0530

    updated app.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-import {User} from "./models/user.model.js"
+import {User} from "../models/user.model.js"
 import jwt from "jsonwebtoken"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/index.js";
+import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 dotenv.config();
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use("/users", userRouter);
+
+
 connectDB();
 
+
 app.get("/", (req, res) => {
-    res.send("Bingo ⚽");
-})
+  res.send("Bingo ⚽");
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
